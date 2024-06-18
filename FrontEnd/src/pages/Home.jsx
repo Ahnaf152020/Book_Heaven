fa04f371a117525e3f9ea7a4b2bf47da90942008
Ahnaf <ahnafamer15@gmail.com>
2024-06-18 20:28:38 +0600

    added homebanner

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Hero from '../components/Home/Hero';
 
 const Home = () => {
+  useEffect(()=> {
+    window.scrollTo(0,0);
+  },[]);
+  
   return (
-    <div>Home</div>
-  )
-}
+     <div className='px-10 py-8 text-white bg-zinc-900'>
+    <Hero />
+  </div>
+  );
+};
 
 export default Home
