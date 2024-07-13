82dacbf9467e46f8ded7db4d6d31257ffe399aa9
Ahnaf <ahnafamer15@gmail.com>
2024-07-13 13:44:35 +0600

    .env added for frontend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
-
 import Cart from './pages/Cart';
 import ChangePassword from './pages/ChangePassword';
 import Profile from './pages/Profile';
@@ -11,35 +10,33 @@ import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Footer from './components/Footer/Footer';
 import BookDetails from './pages/BookDetails';
-import BorrowedBooksPage from './pages/BorrowedBookPage'; 
-import { AuthProvider } from './components/context/authcontext';
+import BorrowedBooksPage from './pages/BorrowedBookPage';
+import { AuthProvider } from '././components/context/authcontext';
+
 const App = () => {
-  
   const [borrowedBooks, setBorrowedBooks] = useState([]);
 
   return (
     <AuthProvider>
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
-         
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/changepassword" element={<ChangePassword />} />
-            <Route path="/book/:id" element={<BookDetails />} />
-            { }
-            <Route path="/borrowed" element={<BorrowedBooksPage borrowedBooks={borrowedBooks} />} />
-          </Routes>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about-us" element={<AboutUs />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/changepassword" element={<ChangePassword />} />
+              <Route path="/book/:id" element={<BookDetails />} />
+              <Route path="/borrowed" element={<BorrowedBooksPage borrowedBooks={borrowedBooks} />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
     </AuthProvider>
   );
 };
