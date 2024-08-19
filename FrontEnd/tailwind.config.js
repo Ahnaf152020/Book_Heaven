be3ac3bf96250e35b9f6cd30eeb953dd84414638
khalidRatin <153265332+khalidRatin@users.noreply.github.com>
2024-08-19 21:20:44 +0600

    Changes in config for moving animation

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        moveBackground: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-80%)' },
+        },
+      },
+      animation: {
+        moveBackground: 'moveBackground 60s linear infinite',
+      },
+    },
   },
   plugins: [],
 };
